eb4ef3bd636d3cead48f048c000b008e439685f4
sadat <you@example.com>
2026-07-02 11:11:31 +0000

    oidc demo

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from Platform Team!"
+    return "Hello from Demo Team!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
